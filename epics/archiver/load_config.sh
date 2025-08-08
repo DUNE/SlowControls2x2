021320508dc7bf1f97606b86c7efbb85e9e7d86f
Dune <acdcs@acd-daq05.fnal.gov>
2025-08-07 21:00:27 -0500

    updating load_config.sh file with most recent xml file

--- epics/archiver/load_config.sh
+++ epics/archiver/load_config.sh
@@ -5,4 +5,4 @@
 source ~/2x2/EPICS/setup_EPICS.sh
 cd /home/acd/acdcs/2x2/EPICS/phoebus/phoebus/services/archive-engine
 
-./archive-engine.sh -engine ND2x2 -port 4812 -import /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/config/ND2x2_archiver_config_20250805.xml -replace_engine
+./archive-engine.sh -engine ND2x2 -port 4812 -import /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/config/ND2x2_archiver_config_20250806.xml -replace_engine
